93abde736e1f723204366327e745db65d83e4c01
Dan Serbyn <dserbyn@agileassets.com>
2019-05-17 22:33:02 +0300

    SKAMAIN-3 Add info document endpoint

---
+++ src/main/java/com/devserbyn/skemex/service/impl/InfoServiceImpl.java
@@ -0,0 +1,26 @@
+package com.devserbyn.skemex.service.impl;
+
+import com.devserbyn.skemex.service.InfoService;
+
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@PropertySources({
+        @PropertySource("classpath:info.properties")
+})
+public class InfoServiceImpl implements InfoService
+{
+
+    private final Environment env;
+
+    @Override
+    public String getInfoDocumentURL() {
+        return env.getProperty("info.doc.infoDocumentURL");
+    }
+}
