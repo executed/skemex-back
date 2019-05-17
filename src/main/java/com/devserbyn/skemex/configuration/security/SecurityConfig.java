3710d2745b8399988fed259f23f0d1f611506028
Dan Serbyn <dserbyn@agileassets.com>
2019-05-17 22:33:59 +0300

    SKMAIN-1 Add sleep protecting Cron job

--- src/main/java/com/devserbyn/skemex/configuration/security/SecurityConfig.java
+++ src/main/java/com/devserbyn/skemex/configuration/security/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .antMatchers("/index.html", "/", "/home"
-                        ,"/favicon.ico","/*.js","/*.css","/*.png").permitAll()
+                        ,"/favicon.ico","/*.js","/*.css","/*.png", "/info/infoDocURL").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
