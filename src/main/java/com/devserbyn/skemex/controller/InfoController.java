db5ccaf73e7227dacc4005b46a8fdff1ed1cac0b
Dan Serbyn <dserbyn@agileassets.com>
2019-05-20 14:16:35 +0300

    SKMAIN-3 Change URL for info doc

--- src/main/java/com/devserbyn/skemex/controller/InfoController.java
+++ src/main/java/com/devserbyn/skemex/controller/InfoController.java
@@ -3,6 +3,7 @@ package com.devserbyn.skemex.controller;
 import com.devserbyn.skemex.controller.dto.Response;
 import com.devserbyn.skemex.service.InfoService;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -11,14 +12,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 @RequestMapping("/info")
 public class InfoController {
 
@@ -31,21 +35,22 @@ public class InfoController {
     }
 
     @GetMapping("/infoDoc")
-    public ResponseEntity<byte[]> getPDF() {
-        byte[] contents = new byte[0];
-        try {
-            contents = Files.readAllBytes(Paths.get("/Conflict of Interest Policy.pdf"));
-        } catch (IOException e) {
-            e.printStackTrace();
+    public ResponseEntity<byte[]> getPDF() throws IOException {
+        File resultFile = new ClassPathResource("Conflict of Interest Policy.pdf").getFile();
+        String resultFileName = resultFile.getName();
+
+        if (!resultFile.exists()) {
+            throw new RuntimeException("Info file doesn't exist");
         }
 
         HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_PDF);
-        String filename = "Conflict of Interest Policy.pdf";
-        headers.setContentDispositionFormData(filename, filename);
+        headers.setContentType(MediaType.parseMediaType("application/pdf"));
+        headers.add("content-disposition", "inline;filename=" + resultFileName);
+        headers.setContentDispositionFormData(resultFileName, resultFileName);
         headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
-        ResponseEntity<byte[]> response = new ResponseEntity<>(contents, headers, HttpStatus.OK);
 
-        return response;
+        byte[] resultFileBytes = Files.readAllBytes(resultFile.toPath());
+
+        return new ResponseEntity<>(resultFileBytes, headers, HttpStatus.OK);
     }
 }
